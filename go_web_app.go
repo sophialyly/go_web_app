7aab853c4fd01f5a1491a340bcba44c672fe6d95
keviny22 <kevin_young@intuit.com>
2016-03-15 13:31:38 -0700

    update

--- go_web_app.go
+++ go_web_app.go
@@ -2,55 +2,74 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 	//"io/ioutil"
 	//"bufio"
 	//"os"
 	//"strings"
 )
 
-//func main() {
-//	//http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){  //passes anonymous function
-//	//	w.Write([]byte("hello World"))
-//	//})
-//	http.Handle("/", new(MyHandler))
-//
-//	http.ListenAndServe(":8000", nil) //second arg enables multiplexing
-//}
-//
-//type MyHandler struct {
-//	http.Handler
-//}
-//
-//func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-//	path := "public/" + req.URL.Path
-//	f, err := os.Open(path)
-//	//data, err := ioutil.ReadFile(string(path)) unbuffered reading of content, huge memory hog
-//
-//	if err == nil {
-//		bufferedReader := bufio.NewReader(f)
-//		var contentType string
-//		if strings.HasSuffix(path, ".css") {
-//			contentType = "text/css"
-//		} else if strings.HasSuffix(path, ".html") {
-//			contentType = "text/html"
-//		} else if strings.HasSuffix(path, ".js") {
-//			contentType = "application/javascript"
-//		} else if strings.HasSuffix(path, ".png") {
-//			contentType = "image/png"
-//		} else if strings.HasSuffix(path, ".mp4") {
-//			contentType = "video/mp4"
-//		} else {
-//			contentType = "text/plain"
-//		}
-//		w.Header().Add("Content-Type", contentType)
-//		bufferedReader.WriteTo(w)
-//		//w.Write(data)
-//	} else {
-//		w.Write([]byte("404 - " + http.StatusText(404)))
-//	}
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
+		w.Header().Add("Content Type","text/html")
+		//tmpl, err := template.New("test").Parse(doc)
+		templates := template.New("template")
+		templates.New("test").Parse(doc)
+		templates.New("header").Parse(header)
+		templates.New("footer").Parse(footer)
+		//if err == nil {
+			context := Context{
+				[3]string{"Lemon", "Orange", "Apple"},
+				"the title",
+			}
+			//tmpl.Execute(w,context)
+			templates.Lookup("test").Execute(w, context)
+		//}
+
+	})
+	//http.HandleFunc("/", render)
+	http.ListenAndServe(":8000", nil)
+}
+
+func render(w http.ResponseWriter, req *http.Request){
+	w.Header().Add("Content Type","text/html")
+	tmpl, err := template.New("test").Parse(doc)
+	if err == nil {
+		//context := Context{"the message"}
+		//tmpl.Execute(w,context)
+		tmpl.Execute(w,req.URL.Path)
+	}
+}
+
+//type Context struct  {
+//	Message string
 //
 //}
 
-func main(){
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+const doc = `
+{{template "header" .Title}}
+ <body>
+  <h1>List of Fruits</h1>
+  <ul>
+    {{range .Fruit}}
+      <li>{{.}}</li>
+    {{end}}
+  </ul>
+ </body>
+ {{template "footer"}}
+`
+
+const header = `
+<!DOCTYPE html>
+<html>
+ <head><title>{{.}}</title></head>
+ `
+
+const footer = `
+</html>
+`
+
+type Context struct {
+	Fruit [3] string
+	Title string
 }
